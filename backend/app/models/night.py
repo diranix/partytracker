e288252c386ac2dbac38cf058d20dca93481e725
Antonioioioio <antoninatomchuk10@gmail.com>
2026-03-31 18:16:57 +0200

    Add PostgreSQL setup and nights model

---
+++ backend/app/models/night.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String
+from backend.app.db.database import Base
+
+
+class Night(Base):
+    __tablename__ = "nights"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
+    location = Column(String, nullable=False)
+    mood = Column(String, nullable=False)
