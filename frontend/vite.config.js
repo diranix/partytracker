66bacfbe627b9b9aea661cfd476f0f7e733ccf0f
Ivan <Diranix0@icloud.com>
2026-04-09 09:38:49 +0200

    фікс vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   server: {
     host: true,
     port: 3000,
+    allowedHosts: ['frontend', 'localhost', 'partytracker.fun'],
     proxy: {
       '/api': {
         target: 'http://backend:8000',
