7f5977662014a0984d6947246132f06f31bf76c4
Ivan <Diranix0@icloud.com>
2026-04-11 21:54:46 +0200

    front and ci fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ['frontend', 'localhost'],
+    allowedHosts: ['frontend', 'localhost', 'frontend-dev', 'dev.partytracker.fun'],
     host: '0.0.0.0',
     port: 3000,
     allowedHosts: ['frontend', 'localhost', 'partytracker.fun'],
